380ea806f95c555fe45207b33c3e7822f561a1e0
Gabriel Pinheiro <108769280+GabrielPinheiroC@users.noreply.github.com>
2024-04-29 20:31:50 -0300

    feat: implementando percorrer_lista_de_usuarios.js

---
+++ Aula-6/percorrer_lista_de_usuarios.js
@@ -0,0 +1,9 @@
+// 1 - Utilizando ForLoop e array juntos, faça uma lista com 5 nomes, e faça o forLoop percorrer por ela exibindo todos os nomes um por um.
+// Será necessário um método de array chamado "length".
+
+const usuarios = ["Sofia", "Mateus", "Isabelle", "Lucas", "Alice"];
+
+for (let i = 0; i < usuarios.length; i++) {
+    console.log(usuarios[i]);
+   
+}
